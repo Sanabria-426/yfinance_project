2ff169c53e9eae7ac83a3bb788ddf852e3072750
Sanabria <le_sanabria@protonmail.com>
2026-04-06 16:26:18 +0200

    Tabs added for clarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 
 ### @What to do next?
 # Allow to decide of the interval
-# Make the date range apply to oher data
 #
 
 
@@ -23,82 +22,86 @@ def search_function(searchterm: str):
     # @TODO: Styling for the elements in the searchbar
     # @TODO: Workaround to display nicely and return efficiently
     # Thought: Maybe parse the string once received. I need only the symbol after all
-    # @TODO: Error when no result - check on that
+    # @TODO: Error when no result - check on that - That might be because the value for symbols was retrieved without get
     # @TODO: Check if the numbers (millions) could be more readable
 
     return [
         {"Symbol": i.get('symbol'), "Short name": i.get('shortname')}
     for i in tickers ]
 
-# @TODO: Maybe provide a sidebar for the user to clearly identify what is displayed
 def get_financial_data(symbol: str):
     # Create a Ticker object
     ticker = yf.Ticker(symbol)
 
     # Fetch historical market data for the last year
 
-    start_date, end_date = date_range_picker(
-        "Select a range for the historical data",
-        default_start=date.today() - timedelta(days=30),
-        default_end=date.today(),
-        min_date=date(1970, 1, 1),
-        max_date=date.today(),
-        error_message="Please select start and end date"
-    )
+    with historical_data_tab:
 
-    st.write(start_date, end_date)
+        start_date, end_date = date_range_picker(
+            "Select a range for the historical data",
+            default_start=date.today() - timedelta(days=30),
+            default_end=date.today(),
+            min_date=date(1970, 1, 1),
+            max_date=date.today(),
+            error_message="Please select start and end date"
+        )
 
-    historical_data = ticker.history(start=start_date, end=end_date)
+        st.write(start_date, end_date)
 
-    # Display a summary of the fetched data
-    st.write("Historical Data:")
-    st.write(historical_data[['Open', 'High', 'Low', 'Close', 'Volume']])
+        historical_data = ticker.history(start=start_date, end=end_date)
 
-    # Build a figure with plotly. A bit different from the YT tutorial
-    # new_frame = pd.DataFrame(historical_data)
-    # fig = go.Figure(data=[go.Candlestick(x=new_frame.index,
-    #                                      open=new_frame['Open'], high=new_frame['High'],
-    #                                      low=new_frame['Low'], close=new_frame['Close'])])
-    # st.plotly_chart(fig)
+        # Display a summary of the fetched data
+        st.write("Historical Data:")
+        st.write(historical_data[['Open', 'High', 'Low', 'Close', 'Volume']])
+
+        # Build a figure with plotly. A bit different from the YT tutorial
+        # new_frame = pd.DataFrame(historical_data)
+        # fig = go.Figure(data=[go.Candlestick(x=new_frame.index,
+        #                                      open=new_frame['Open'], high=new_frame['High'],
+        #                                      low=new_frame['Low'], close=new_frame['Close'])])
+        # st.plotly_chart(fig)
 
 
-    # Apparently there was no need for dataframe
-    fig2 = go.Figure()
-    fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['High'],
-                              mode='lines',
-                              name='High', line=dict(color='green')))
-    # fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Low'],
-    #                           mode='lines',
-    #                           name='20th percentile', line=dict(color='red', dash='dash')))
-    # fig2.add_trace(go.Scatter(x=historical_data.index, y=(historical_data['Close'] - historical_data['Open']),
-    #                           mode='lines+markers',
-    #                           name='20th percentile', line=dict(color='blue', dash='dash')))
-    # 'Volume' is not on the same scale.
-    # If displayed with the others, the chart is harder to read
-    # fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
-    #                           mode='lines',
-    #                           name='20th percentile', line=dict(color='blue', dash='dash')))
-    st.plotly_chart(fig2)
+        # Apparently there was no need for dataframe
+        fig2 = go.Figure()
+        fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['High'],
+                                  mode='lines',
+                                  name='High', line=dict(color='green')))
+        # fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Low'],
+        #                           mode='lines',
+        #                           name='20th percentile', line=dict(color='red', dash='dash')))
+        # fig2.add_trace(go.Scatter(x=historical_data.index, y=(historical_data['Close'] - historical_data['Open']),
+        #                           mode='lines+markers',
+        #                           name='20th percentile', line=dict(color='blue', dash='dash')))
+        # 'Volume' is not on the same scale.
+        # If displayed with the others, the chart is harder to read
+        # fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
+        #                           mode='lines',
+        #                           name='20th percentile', line=dict(color='blue', dash='dash')))
+        st.plotly_chart(fig2)
 
-    st.write("Volume")
+        st.write("Volume")
 
-    # @TODO: You can zoom on charts. but the problem is, you do not control how you can zoom.
-    # Which sometimes makes the charts harder to read. We should at least try to do something about it.
-    volume_chart = go.Figure()
-    volume_chart.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
-                              mode='lines',
-                              name='Voume', line=dict(color='blue')))
-    st.plotly_chart(volume_chart)
+        # @TODO: You can zoom on charts. but the problem is, you do not control how you can zoom.
+        # Which sometimes makes the charts harder to read. We should at least try to do something about it.
+        volume_chart = go.Figure()
+        volume_chart.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
+                                  mode='lines',
+                                  name='Volume', line=dict(color='blue')))
+        st.plotly_chart(volume_chart)
 
     # Fetch basic financials
-    financials = ticker.financials
-    st.write("\nFinancials:")
-    st.write(financials)
+    with financials_tab:
+        financials = ticker.financials
+        st.write("\nFinancials:")
+        st.write(financials)
 
     # Fetch stock actions like dividends and splits
-    actions = ticker.actions
-    st.write("\nStock Actions:")
-    st.write(actions)
+
+    with stock_actions_tab:
+        actions = ticker.actions
+        st.write("\nStock Actions:")
+        st.write(actions)
 
 selected_value = st_searchbox(
     search_function,
@@ -107,27 +110,10 @@ selected_value = st_searchbox(
     debounce=250  # Delays callback by 250ms
 )
 
+
 if selected_value:
     st.write(f"Selected: {selected_value.get("Symbol")} | {selected_value.get("Short name")}")
+
+    historical_data_tab, financials_tab, stock_actions_tab = st.tabs(["Historical Data", "Financials", "Stock Actions"])
+
     get_financial_data(selected_value.get("Symbol"))
-
-
-
-### Very simple search bar
-# ticker = st.text_input("First name")
-# st.write(ticker)
-
-### How to search for a ticker with yf
-# tickers = yf.Search("AA", max_results=10).quotes
-# print(tickers)
-
-###
-
-# Roadmap
-
-# Step 1: Get a ticker and display data about it
-# @TODO List:
-# Take care of suggestions when searching for a symbol / ticker
-### Searchbar:
-
-###
