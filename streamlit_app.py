785122bfad8cb66fd88d2ec6af7ce339287c1f19
Sanabria <le_sanabria@protonmail.com>
2026-04-07 16:04:50 +0200

    Add Close Price, MA20/50, alerts

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,50 +67,95 @@ def interval_select_box(start_date, end_date):
 
     return interval_translator[option]
 
+@st.fragment(run_every="1m")
+def get_historical_data(ticker):
+    start_date, end_date = date_range_picker(
+        "Select a range for the historical data",
+        default_start=date.today() - timedelta(days=30),
+        default_end=date.today(),
+        min_date=date(1970, 1, 1),
+        max_date=date.today(),
+        error_message="Please select start and end date"
+    )
+
+    interval = interval_select_box(start_date, end_date)
+
+    # Historical Data: hd
+    hd = ticker.history(start=start_date, end=end_date + timedelta(days=1), interval=interval)
+
+    # Display a summary of the fetched data
+    # @TODO: Do so that you retrieve enough data to calculate the starting values
+    st.write("Historical Data:", hd[['Open', 'High', 'Low', 'Close', 'Volume']].iloc[::-1])
+
+    # 20-Day and 50-Day Moving Averages
+    hd["MA20"] = hd["Close"].rolling(window=20).mean()
+    hd["MA50"] = hd["Close"].rolling(window=50).mean()
+
+    st.write("Other values", hd[['Dividends', 'Stock Splits', 'MA20', 'MA50']].iloc[::-1])
+
+    # Build a figure with plotly. A bit different from the YT tutorial
+    # new_frame = pd.DataFrame(historical_data)
+    # fig = go.Figure(data=[go.Candlestick(x=new_frame.index,
+    #                                      open=new_frame['Open'], high=new_frame['High'],
+    #                                      low=new_frame['Low'], close=new_frame['Close'])])
+    # st.plotly_chart(fig)
+    hd["price_change"] = hd["Close"].pct_change()  # daily % change
+
+    hd["z_volume"] = (hd["Volume"] - hd["Volume"].mean()) / hd["Volume"].std()
+    hd["z_price"] = (hd["price_change"] - hd["price_change"].mean()) / hd["price_change"].std()
+
+    # @TODO: You can zoom on charts. but the problem is, you do not control how you can zoom.
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=hd.index, y=hd['Close'],
+                              mode='lines',
+                              name='Close Price', line=dict(color='green')))
+    fig2.add_trace(go.Scatter(x=hd.index, y=hd['MA20'],
+                              mode='lines',
+                              name='20-Day Moving Averages', line=dict(color='orange')))
+    fig2.add_trace(go.Scatter(x=hd.index, y=hd['MA50'],
+                              mode='lines',
+                              name='50-Day Moving Averages', line=dict(color='blue')))
+    if interval is "1d":
+        # Flag a day as anomalous if either Z-score exceeds 2
+        hd["anomaly"] = (hd["z_volume"].abs() > 2) | (hd["z_price"].abs() > 2)
+        anomalies = hd[hd["anomaly"] == True]
+        anomalies["hover_text"] = (
+                "Z-volume: " + hd["z_volume"].round(2).astype(str) + "<br>" +
+                "Z-price: " + hd["z_price"].round(2).astype(str)
+        )
+
+        fig2.add_trace(go.Scatter(x=anomalies.index, y=anomalies["Close"],
+            mode="markers", name="Anomaly", marker=dict(color="red", size=5, symbol="circle"),
+            text=anomalies["hover_text"],
+            hoverinfo="text+x+y",
+        ))
+    st.plotly_chart(fig2)
+
+    fig3 = go.Figure()
+    fig3.add_trace(go.Scatter(x=hd.index, y=hd['z_volume'],
+                              mode='lines',
+                              name='Daily Volumes', line=dict(color='blue')))
+    fig3.add_trace(go.Scatter(x=hd.index, y=hd['z_price'],
+                              mode='lines',
+                              name='Daily Price Change', line=dict(color='green')))
+
+    st.plotly_chart(fig3)
+
+    # st.write("Volumes")
+    #
+    # # Which sometimes makes the charts harder to read. We should at least try to do something about it.
+    # volume_chart = go.Figure()
+    # volume_chart.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
+    #                                   mode='lines',
+    #                                   name='Volume', line=dict(color='blue')))
+    # st.plotly_chart(volume_chart)
+
 def get_financial_data(symbol: str):
 
     ticker = yf.Ticker(symbol)
 
     with historical_data_tab:
-        start_date, end_date = date_range_picker(
-            "Select a range for the historical data",
-            default_start=date.today() - timedelta(days=30),
-            default_end=date.today(),
-            min_date=date(1970, 1, 1),
-            max_date=date.today(),
-            error_message="Please select start and end date"
-        )
-
-        option = interval_select_box(start_date, end_date)
-
-        historical_data = ticker.history(start=start_date, end=end_date + timedelta(days=1), interval=option)
-
-        # Display a summary of the fetched data
-        st.write("Historical Data:")
-        st.write((historical_data[['Open', 'High', 'Low', 'Close', 'Volume']].iloc[::-1]))
-
-        # Build a figure with plotly. A bit different from the YT tutorial
-        # new_frame = pd.DataFrame(historical_data)
-        # fig = go.Figure(data=[go.Candlestick(x=new_frame.index,
-        #                                      open=new_frame['Open'], high=new_frame['High'],
-        #                                      low=new_frame['Low'], close=new_frame['Close'])])
-        # st.plotly_chart(fig)
-
-        fig2 = go.Figure()
-        fig2.add_trace(go.Scatter(x=historical_data.index, y=historical_data['High'],
-                                  mode='lines',
-                                  name='High', line=dict(color='green')))
-        st.plotly_chart(fig2)
-
-        st.write("Volume")
-
-        # @TODO: You can zoom on charts. but the problem is, you do not control how you can zoom.
-        # Which sometimes makes the charts harder to read. We should at least try to do something about it.
-        volume_chart = go.Figure()
-        volume_chart.add_trace(go.Scatter(x=historical_data.index, y=historical_data['Volume'],
-                                  mode='lines',
-                                  name='Volume', line=dict(color='blue')))
-        st.plotly_chart(volume_chart)
+        get_historical_data(ticker)
 
     # Fetch basic financials
     with financials_tab:
